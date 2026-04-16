49b74c5ecc8ba7fe64d2ba4414c82de2fd5954ab
Liveiciee <liveiciee@email.com>
2026-04-16 09:44:13 +0800

    chore: simplify build.zig

--- build.zig
+++ build.zig
@@ -5,24 +5,20 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "tinycross",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
+       .name = "tinycross",
+       .root_module = b.createModule(.{
+           .root_source_file = b.path("src/main.zig"),
+           .target = target,
+           .optimize = optimize,
         }),
     });
-
-    // Add args module
-    const args_mod = b.addModule("args", .{
-        .root_source_file = b.path("src/args.zig"),
-    });
-    exe.root_module.addImport("args", args_mod);
 
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| run_cmd.addArgs(args);
+
     const run_step = b.step("run", "Run tinycross");
     run_step.dependOn(&run_cmd.step);
 }
